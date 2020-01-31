afd76b90e9ef322d411986af1827b767962d629b
Hadyn <hadyn.richard@gmail.com>
2020-01-30 23:22:00 -0700

    Added DecryptFileArchive/ArchiveFileLength and tests

--- jagex/file_archive.go
+++ jagex/file_archive.go
@@ -5,20 +5,37 @@ import (
     "golang.org/x/crypto/xtea"
 )
 
-type ArchiveCompression uint8
-
 const (
-    FileArchiveCompressionNone ArchiveCompression = iota
-    FileArchiveCompressionBZIP
-    FileArchiveCompressionGZIP
+    minimumArchiveHeaderLength = 5
 )
 
-func (ac ArchiveCompression) valid() error {
-    if ac < FileArchiveCompressionNone || ac > FileArchiveCompressionGZIP {
+type ArchiveCompression byte
+
+const (
+    ArchiveCompressionNone ArchiveCompression = iota
+    ArchiveCompressionBZIP
+    ArchiveCompressionGZIP
+)
+
+func (c ArchiveCompression) Validate() error {
+    if c < ArchiveCompressionNone || c > ArchiveCompressionGZIP {
         return errors.New("unrecognized compression")
     }
     return nil
 }
+
+func (c ArchiveCompression) HeaderLength() int {
+    switch c {
+    case ArchiveCompressionNone:
+        return minimumArchiveHeaderLength
+    case ArchiveCompressionBZIP, ArchiveCompressionGZIP:
+        return minimumArchiveHeaderLength + 4
+    default:
+        panic("unhandled compression enumeration")
+    }
+}
+
+func (c ArchiveCompression) AsByte() byte { return byte(c) }
 
 func DecompressFileArchive(bs []byte) ([]byte, error) {
     return nil, nil
@@ -30,6 +47,22 @@ func DecryptFileArchive(bs []byte, key []byte) ([]byte, error) {
         return nil, err
     }
 
-    cipher.Decrypt(nil, nil)
-    return nil, nil
+    copied := make([]byte, len(bs))
+    copy(copied, bs[:minimumArchiveHeaderLength])
+
+    archiveLength := FileArchiveLength(bs)
+    for i := minimumArchiveHeaderLength; i < archiveLength-xtea.BlockSize; i += xtea.BlockSize {
+        cipher.Decrypt(copied[i:], bs[i:i+xtea.BlockSize])
+    }
+
+    return copied, nil
+}
+
+func FileArchiveLength(bs []byte) int {
+    var (
+        rb = ReadBuffer(bs)
+        c  = ArchiveCompression(rb.GetUint8())
+    )
+
+    return c.HeaderLength() + rb.GetUint32AsInt()
 }
---
+++ jagex/file_archive_test.go
@@ -0,0 +1,104 @@
+package jagex
+
+import (
+    "fmt"
+    "github.com/stretchr/testify/assert"
+    "testing"
+)
+
+func TestDecryptFileArchive(t *testing.T) {
+    tests := []struct {
+        name      string
+        giveBytes []byte
+        giveKey   []byte
+        wantBytes []byte
+        wantError    bool
+        wantErrorMsg string
+    }{
+        {
+            name:      "uncompressed boundary",
+            giveBytes: []byte{},
+            giveKey:   []byte{},
+            wantBytes: []byte{},
+        },
+        {
+            name:      "uncompressed off boundary",
+            giveBytes: []byte{},
+            giveKey:   []byte{},
+            wantBytes: []byte{},
+        },
+    }
+
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            decrypted, err := DecryptFileArchive(tt.giveBytes, tt.giveKey)
+
+            if tt.wantError {
+                assert.EqualError(t, err, tt.wantErrorMsg)
+            } else {
+                assert.NoError(t, err)
+                assert.Equal(t, tt.wantBytes, decrypted)
+            }
+        })
+    }
+}
+
+func TestFileArchiveLength(t *testing.T) {
+    tests := []struct {
+        name       string
+        give       []byte
+        wantLength int
+        wantPanic  bool
+    }{
+        {
+            name:       "uncompressed",
+            give:       []byte{ArchiveCompressionNone.AsByte(), 0, 0, 0, 1, 1},
+            wantLength: 6,
+            wantPanic:  false,
+        },
+        {
+            name:       "bzip",
+            give:       []byte{ArchiveCompressionBZIP.AsByte(), 0, 0, 0, 1, 0, 0, 0, 2, 1},
+            wantLength: 10,
+            wantPanic:  false,
+        },
+        {
+            name:       "gzip",
+            give:       []byte{ArchiveCompressionGZIP.AsByte(), 0, 0, 0, 1, 0, 0, 0, 2, 1},
+            wantLength: 10,
+            wantPanic:  false,
+        },
+        {
+            name:      "insufficient bytes uncompressed",
+            give:      []byte{ArchiveCompressionNone.AsByte(), 0, 0, 0,},
+            wantPanic: true,
+        },
+        {
+            name:      "insufficient bytes bzip",
+            give:      []byte{ArchiveCompressionBZIP.AsByte(), 0, 0, 0,},
+            wantPanic: true,
+        },
+        {
+            name:      "insufficient bytes gzip",
+            give:      []byte{ArchiveCompressionGZIP.AsByte(), 0, 0, 0,},
+            wantPanic: true,
+        },
+        {
+            name:      "unrecognized compression",
+            give:      []byte{255, 0, 0, 0, 1, 1},
+            wantPanic: true,
+        },
+    }
+
+    for _, tt := range tests {
+        t.Run(fmt.Sprintf("%s", tt.name), func(t *testing.T) {
+            defer func() {
+                if r := recover(); r != nil && !tt.wantPanic {
+                    t.Fatal("call panicked when did not want panic")
+                }
+            }()
+
+            assert.Equal(t, tt.wantLength, FileArchiveLength(tt.give))
+        })
+    }
+}
