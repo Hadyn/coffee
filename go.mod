ab4c64d2e977b960ecf59aa69ee6955ecd5019b2
Hadyn <hadyn.richard@gmail.com>
2020-01-31 20:20:58 -0700

    Implemented DecompressFileArchive

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/hadyn/coffee
 go 1.13
 
 require (
+	github.com/dsnet/compress v0.0.1
 	github.com/stretchr/testify v1.4.0
 	golang.org/x/crypto v0.0.0-20200128174031-69ecbb4d6d5d
 )
--- jagex/file_archive.go
+++ jagex/file_archive.go
@@ -1,8 +1,12 @@
 package jagex
 
 import (
+    "bytes"
+    "compress/gzip"
     "errors"
+    "github.com/dsnet/compress/bzip2"
     "golang.org/x/crypto/xtea"
+    "io"
 )
 
 const (
@@ -38,7 +42,56 @@ func (c ArchiveCompression) HeaderLength() int {
 func (c ArchiveCompression) AsByte() byte { return byte(c) }
 
 func DecompressFileArchive(bs []byte) ([]byte, error) {
-    return nil, nil
+    rb := ReadBuffer(bs)
+
+    var (
+        c                = ArchiveCompression(rb.GetUint8())
+        headerLength     = c.HeaderLength()
+        compressedLength = rb.GetUint32AsInt()
+    )
+
+    switch c {
+    case ArchiveCompressionNone:
+        copied := make([]byte, compressedLength)
+        copy(copied, bs[headerLength:headerLength+compressedLength])
+        return copied, nil
+    case ArchiveCompressionBZIP, ArchiveCompressionGZIP:
+        decompressed := make([]byte, rb.GetUint32())
+
+        var r io.Reader
+        switch c {
+        case ArchiveCompressionBZIP:
+            var err error
+            r, err = bzip2.NewReader(
+                io.MultiReader(
+                    bytes.NewReader([]byte("BZh9")),
+                    bytes.NewReader(bs[headerLength:headerLength+compressedLength]),
+                ),
+                &bzip2.ReaderConfig{},
+            )
+
+            if err != nil {
+                return nil, err
+            }
+        case ArchiveCompressionGZIP:
+            var err error
+            r, err = gzip.NewReader(
+                bytes.NewReader(bs[headerLength: headerLength+compressedLength]),
+            )
+
+            if err != nil {
+                return nil, err
+            }
+        }
+
+        if _, err := io.ReadFull(r, decompressed); err != nil {
+            return nil, err
+        }
+
+        return decompressed, nil
+    default:
+        panic("unhandled compression enumeration")
+    }
 }
 
 func DecryptFileArchive(bs []byte, key []byte) ([]byte, error) {
